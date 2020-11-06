765e1791306195946ddb62593c58c2ae1ee5a14f
remydegrave <bolieve.fr@gmail.com>
2020-11-06 11:57:10 +0100

    exercice1pt1Jq

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $('#text').hide();
+// });
+// ou 2ème solution :
+$(function(){
+    //$('elementCiblé').css('propriétéCSS','valeur'); 
+    $('#text').css('visibility','hidden');       
+});
